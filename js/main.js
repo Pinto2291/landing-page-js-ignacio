78e8ca4e66d236748ea476394db5462206af84fb
pinto22 <51886210+Pinto2291@users.noreply.github.com>
2023-11-01 09:43:59 -0400

    mas javascript agregado, html y css modificado, contador y slider agregados

--- js/main.js
+++ js/main.js
@@ -12,3 +12,47 @@ btn_cerrar.addEventListener('click', () => {
     menu_mobile.style.display = 'none';
     menu_mobile_button.style.display = 'flex';
 });
+
+/* Slider */
+
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.sub-part');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const counter = document.querySelector('.counter');
+
+let currentSlide = 0;
+const slideCount = slides.length;
+
+// Hide all slides except the first one
+slides.forEach((slide, index) => {
+  if (index !== currentSlide) {
+    slide.style.display = 'none';
+  }
+});
+
+// Update the counter
+counter.textContent = `${currentSlide + 1} / ${slideCount}`;
+
+// Add event listeners to the buttons
+prevBtn.addEventListener('click', () => {
+  // Hide the current slide
+  slides[currentSlide].style.display = 'none';
+  // Update the current slide
+  currentSlide = (currentSlide - 1 + slideCount) % slideCount;
+  // Show the new current slide
+  slides[currentSlide].style.display = 'grid';
+  // Update the counter
+  counter.textContent = `${currentSlide + 1} / ${slideCount}`;
+});
+
+nextBtn.addEventListener('click', () => {
+  // Hide the current slide
+  slides[currentSlide].style.display = 'none';
+  // Update the current slide
+  currentSlide = (currentSlide + 1) % slideCount;
+  // Show the new current slide
+  slides[currentSlide].style.display = 'grid';
+  // Update the counter
+  counter.textContent = `${currentSlide + 1} / ${slideCount}`;
+});
